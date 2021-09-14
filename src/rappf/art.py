6fa5e44840ee94cd0c66ed45cde33529b6ee1ff3
AP Ljungquist <ap@ljungquist.eu>
2021-09-14 11:16:08 +0200

    Improve art layout
    Previously it would sometimes interleave nodes from two cliques whereas
    now it prefers to keep cliques together. The new implementation makes
    the second example worse but the mistake is not so bad as that which
    has been fixed.
    
    Make flake8 back down in disagreements with black.

--- src/rappf/art.py
+++ src/rappf/art.py
@@ -8,29 +8,70 @@ from typing import (
     Iterable,
     Iterator,
     List,
+    Literal,
     Mapping,
     Optional,
     Sequence,
     Tuple,
     TypeVar,
+    Union,
 )
 
 _HashableT = TypeVar("_HashableT", bound=Hashable)
 _AdjacencyListT = Mapping[_HashableT, Iterable[_HashableT]]
 
 
-def _sorted_topolocial_guided(
-    graph: _AdjacencyListT[_HashableT], reach
-) -> Iterator[_HashableT]:
-    sorter = graphlib.TopologicalSorter(graph)
-    sorter.prepare()
-    ready: List[_HashableT] = []
-    while sorter.is_active():
-        ready.extend(sorter.get_ready())
-        node = min(ready, key=reach.__getitem__)
-        sorter.done(node)
-        ready.remove(node)
-        yield node
+def _score(graph: _AdjacencyListT[_HashableT], order: Sequence[_HashableT]) -> int:
+    return sum(
+        (token[:2] == ("|", "-")) + (token[0] == "|") + (token[2] == "-")
+        for token in _art_hashable_tokens(graph, order)
+        if isinstance(token, tuple)
+    )
+
+
+def _refined_topolocial(
+    graph: _AdjacencyListT[_HashableT], start: Sequence[_HashableT]
+) -> Sequence[_HashableT]:
+    # This is ridiculously slow for non-trivial examples
+    # It is also prone to getting stuck in local optima since it only considers moving
+    # one node at a time.
+    curr_order = tuple(start)
+    curr_score = _score(graph, curr_order)
+    prev_score = curr_score + 1
+    while curr_score < prev_score:
+        prev_score = curr_score
+        prev_order = curr_order
+        # Move forward
+        for cut_at, cut in enumerate(prev_order):
+            left_of_cut, right_of_cut = prev_order[:cut_at], prev_order[(cut_at + 1) :]
+            for move_by, left_neighbor in enumerate(right_of_cut, 1):
+                left_of_insert, right_of_insert = (
+                    right_of_cut[:move_by],
+                    right_of_cut[move_by:],
+                )
+                if cut in graph.get(left_neighbor, []):
+                    break  # Maintain topological order
+                order = left_of_cut + left_of_insert + (cut,) + right_of_insert
+                score = _score(graph, order)
+                if score <= curr_score:
+                    curr_score = score
+                    curr_order = order
+        # Move backward
+        for cut_at, cut in list(enumerate(prev_order))[::-1]:
+            left_of_cut, right_of_cut = prev_order[:cut_at], prev_order[(cut_at + 1) :]
+            for move_by, right_neighbor in list(enumerate(left_of_cut, 1))[::-1]:
+                left_of_insert, right_of_insert = (
+                    left_of_cut[: move_by - 1],
+                    left_of_cut[move_by - 1 :],
+                )
+                if right_neighbor in graph.get(cut, []):
+                    break
+                order = left_of_insert + (cut,) + right_of_insert + right_of_cut
+                score = _score(graph, order)
+                if score < curr_score:
+                    curr_score = score
+                    curr_order = order
+    return curr_order
 
 
 def _sorted_topological(graph: _AdjacencyListT[_HashableT]) -> Tuple[_HashableT, ...]:
@@ -41,26 +82,7 @@ def _sorted_topological(graph: _AdjacencyListT[_HashableT]) -> Tuple[_HashableT,
     * attempts to minimize the length distance between adjacent nodes.
     """
     sorter = graphlib.TopologicalSorter(graph)
-    # Greedy attempt at reducing the length of edges in the art
-    # So far it minimizes the vertical component but ignores the horizontal component.
-    direct_successors_lists = _inverted(graph)
-    curr = tuple(sorter.static_order())
-    for i in range(10):
-        positions = {k: i for i, k in enumerate(curr)}
-        reaches = {
-            predecessor: max(
-                [
-                    positions[successor]
-                    for successor in direct_successors_lists.get(predecessor, [])
-                ],
-                default=0,
-            )
-            for predecessor in curr
-        }
-        prev, curr = curr, tuple(_sorted_topolocial_guided(graph, reaches))
-        if prev == curr:
-            return curr
-    raise RuntimeError("Failed to sort nodes in 10 rounds")
+    return tuple(_refined_topolocial(graph, tuple(sorter.static_order())))
 
 
 def art(
@@ -69,20 +91,8 @@ def art(
     fmt: Callable[[_HashableT], str] = lambda x: str(x),
 ) -> str:
     """Return ascii art representation of graph"""
-    positions = {v: i for i, v in enumerate(_sorted_topological(graph))}
-    names = [fmt(v) for v in positions]
-    predecessor_lists = {
-        positions[src]: [positions[dst] for dst in dsts] for src, dsts in graph.items()
-    }
-    sucessor_lists = _inverted(predecessor_lists)
-    return "".join(
-        _art(
-            names,
-            predecessor_lists,
-            sucessor_lists,
-            max(map(len, names)) if max_col_width is None else max_col_width,
-        )
-    )
+    order = _sorted_topological(graph)
+    return "".join(_art_str_tokens(graph, order, fmt, max_col_width))
 
 
 def _above_has_successor_to_the_right(
@@ -99,21 +109,56 @@ def _above_has_successor_to_the_right(
         return False
 
 
-def _art(
-    names: Sequence[str],
-    predecessor_lists: Mapping[int, Iterable[int]],
-    successor_lists: Mapping[int, Iterable[int]],
-    max_width: int,
+def _art_str_tokens(
+    graph: _AdjacencyListT[_HashableT],
+    order,
+    fmt,
+    max_col_width,
 ) -> Iterator[str]:
-    col_widths = [min(max_width, len(x)) for x in names]
-    for row in range(len(names)):
+    tokens = _art_hashable_tokens(graph, order)
+    names = {v: fmt(v) for v in order}
+    reserved = {"-", " ", ""}
+    assert not reserved & set(names.values())
+    if max_col_width is None:
+        max_col_width = max(map(len, names.values()))
+    col_widths = {
+        col: min(len(names[node]), max_col_width) for col, node in enumerate(order)
+    }
+
+    row = col = 0
+    for token in tokens:
+        if token == "\n":
+            row += 1
+            col = 0
+            yield token
+        else:
+            col_width = col_widths[col]
+            ll, lr, cc, rr = token
+            if cc in reserved:
+                cc = cc * col_width
+            else:
+                cc = f"{names[cc][:col_width]:{col_width}}"
+            yield ll + lr + cc + rr
+            col += 1
+
+
+def _art_hashable_tokens(
+    graph: _AdjacencyListT[_HashableT],
+    order: Sequence[_HashableT],
+) -> Iterator[Union[Literal["\n"], Tuple[str, str, Union[_HashableT, str], str]]]:
+    cc: Union[_HashableT, str]  # help mypy
+
+    positions = {v: i for i, v in enumerate(order)}
+    predecessor_lists = {
+        positions[src]: [positions[dst] for dst in dsts] for src, dsts in graph.items()
+    }
+    successor_lists = _inverted(predecessor_lists)
+    for row in range(len(order)):
         successors = successor_lists.get(row, [])
         if row:
             yield "\n"
-        for col in range(len(names)):
+        for col in range(len(order)):
             predecessors = predecessor_lists.get(col, [])
-            col_width = col_widths[col]
-
             if col < row:
                 ll = lr = cc = rr = " "
             elif row == col:
@@ -123,7 +168,7 @@ def _art(
                 else:
                     ll = lr = " "
 
-                cc = names[row]
+                cc = order[row]
 
                 if successors:
                     rr = "-"
@@ -150,11 +195,10 @@ def _art(
                 else:
                     lr = cc = rr = ""
 
-            if col:
-                yield ll
-                yield lr
-            yield lr * (col_width - len(cc)) + cc[:col_width]
-            yield rr
+            if not col:
+                ll = lr = ""
+
+            yield ll, lr, cc, rr
 
 
 def _inverted(
