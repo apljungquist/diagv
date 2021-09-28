d14f550f69574f0d1f2a179eebc41d5e6a819b5e
AP Ljungquist <ap@ljungquist.eu>
2021-09-28 22:15:41 +0200

    Refactor visualization
    Use a generator instead of a numpy array to have one less dependency
    and because I prefer generators over mutating and returning a result
    object.
    
    Break cell generation into smaller functions so that their name may
    document what they do.

--- src/diagv/visualization.py
+++ src/diagv/visualization.py
@@ -2,14 +2,12 @@
 
 from __future__ import annotations
 
+import dataclasses
 import itertools
-from collections import Mapping
-from typing import Callable, Iterable, Iterator, Optional, Sequence
+from typing import Callable, Generic, Iterable, Iterator, Optional, Sequence, Union
 
 import more_itertools
 import networkx as nx
-import numpy as np
-from numpy.typing import NDArray
 
 from diagv.typing_utils import AdjacencyListT, HashableT, NormalizedAdjacencyList
 
@@ -29,8 +27,9 @@ def text_art(
     if ordering is None:
         ordering = list(nx.topological_sort(digraph))
 
+    col2width = {i: len(fmt(v)) for i, v in enumerate(ordering)}
     cells = _cells(digraph, ordering)
-    return "".join(_fmt_cells(cells, fmt))
+    return "".join(_fmt_cells(cells, col2width, fmt))
 
 
 def _nodes(graph: AdjacencyListT[HashableT]) -> Iterator[HashableT]:
@@ -49,61 +48,6 @@ def _inverted(
     return result
 
 
-def _above_has_successor_to_the_right(
-    row: int,
-    col: int,
-    successor_lists: Mapping[int, Iterable[int]],
-) -> Optional[int]:
-    successors = itertools.chain.from_iterable(
-        successor_lists.get(predecessor, []) for predecessor in range(row)
-    )
-    try:
-        return col < max(successors)
-    except ValueError:
-        return False
-
-
-def _above_has_dpred_to_the_right(
-    row: int,
-    col: int,
-    dpred_lists: Mapping[int, Iterable[int]],
-) -> bool:
-    predecessors = dpred_lists[col]
-    return bool(predecessors and row < max(predecessors))
-
-
-def _has_successor_to_the_right(
-    col: int,
-    successor_lists: Mapping[int, Iterable[int]],
-) -> bool:
-    successors = successor_lists[col]
-    return col < max(successors, default=col)
-
-
-def _has_successor_to_the_left(
-    col: int,
-    successor_lists: Mapping[int, Iterable[int]],
-) -> bool:
-    successors = successor_lists[col]
-    return max(successors, default=col) < col
-
-
-def _right_has_dsucc_before_this(
-    row: int,
-    col: int,
-    dsucc_lists: Mapping[int, Iterable[int]],
-) -> bool:
-    dsuccs = dsucc_lists[row]
-    return bool(dsuccs and min(dsuccs) <= col)
-
-
-def _should_strip_left(
-    col: int,
-    dpred_lists: Mapping[int, Iterable[int]],
-) -> bool:
-    return col == 0 and not dpred_lists[0]
-
-
 class Token(str):
     ...
 
@@ -115,136 +59,190 @@ PADDING = Token(" ")
 NOTHING = Token("")
 
 
-def _cells(digraph: nx.DiGraph, order: Sequence[HashableT]) -> NDArray:
+@dataclasses.dataclass(frozen=True)
+class Cell(Generic[HashableT]):
+    ll: Token
+    lr: Token
+    cc: Union[HashableT, Token]
+    rr: Token
+
+
+def _cells(
+    digraph: nx.DiGraph, order: Sequence[HashableT]
+) -> Iterator[Optional[Cell[HashableT]]]:
     node2position = {v: i for i, v in enumerate(order)}
-    predecessor_lists = {
-        node2position[node]: [
-            node2position[direct_predecessor]
-            for direct_predecessor in direct_predecessors
-        ]
-        for node, direct_predecessors in digraph.pred.items()
+    dpred_lists = {
+        node2position[node]: [node2position[dpred] for dpred in dpreds]
+        for node, dpreds in digraph.pred.items()
     }
-    successor_lists = _inverted(predecessor_lists)
-    num_row = len(order)
-    num_col = 4 * len(order)
-    result: NDArray = np.ndarray((num_row, num_col), dtype=object)
-    for row in range(num_row):
-        for col in range(num_col):
-            result[row, col] = "*"
-    for i, row_node in enumerate(order):
-        successors = set(digraph.successors(row_node))
-        row = i
-        for j, col_node in enumerate(order):
-            predecessors = set(digraph.predecessors(col_node))
-            col = j * 4
-            if j < i:
-                if _should_strip_left(j, predecessor_lists):
-                    result[row, col + 0] = NOTHING
-                elif col_node in successors:
-                    result[row, col + 0] = INTERSECTION
-                elif _above_has_dpred_to_the_right(i, j, predecessor_lists):
-                    result[row, col + 0] = OVERPASS
-                elif _right_has_dsucc_before_this(i, j, successor_lists):
-                    result[row, col + 0] = SECTION
-                else:
-                    result[row, col + 0] = PADDING
+    dsucc_lists = _inverted(dpred_lists)
 
-                if _should_strip_left(j, predecessor_lists):
-                    result[row, col + 1] = NOTHING
-                elif _right_has_dsucc_before_this(i, j, successor_lists):
-                    result[row, col + 1] = SECTION
-                else:
-                    result[row, col + 1] = PADDING
-
-                if _right_has_dsucc_before_this(i, j, successor_lists):
-                    result[row, col + 2] = SECTION
-                    result[row, col + 3] = SECTION
-                else:
-                    result[row, col + 2] = PADDING
-                    result[row, col + 3] = PADDING
-
-            elif i == j:
-                node = row_node
-                assert node == col_node
-                if _should_strip_left(j, predecessor_lists):
-                    result[row, col + 0] = NOTHING
-                    result[row, col + 1] = NOTHING
-                elif predecessors or _has_successor_to_the_left(j, successor_lists):
-                    if predecessors:
-                        result[row, col + 0] = INTERSECTION
-                    else:
-                        result[row, col + 0] = SECTION
-                    result[row, col + 1] = SECTION
-                elif _above_has_successor_to_the_right(i, j, successor_lists):
-                    result[row, col + 0] = PADDING
-                    result[row, col + 1] = PADDING
-
-                result[row, col + 2] = node
-
-                if _has_successor_to_the_right(j, successor_lists):
-                    result[row, col + 3] = SECTION
-                # elif _has_successor_to_the_left(j, successor_lists):
-                #     result[row, col + 3] = SECTION
-                elif _above_has_successor_to_the_right(i, j, successor_lists):
-                    result[row, col + 3] = PADDING
-                else:
-                    result[row, col + 3] = NOTHING
-            else:
-                if _should_strip_left(j, predecessor_lists):
-                    result[row, col + 0] = NOTHING
-                elif row_node in predecessors:
-                    result[row, col + 0] = INTERSECTION
-                elif (
-                    predecessors
-                    and min(map(node2position.__getitem__, predecessors)) < i
-                ):
-                    result[row, col + 0] = OVERPASS
-                elif successors and j < max(map(node2position.__getitem__, successors)):
-                    result[row, col + 0] = SECTION
-                elif _above_has_successor_to_the_right(i, j, successor_lists):
-                    result[row, col + 0] = PADDING
-                else:
-                    result[row, col + 0] = NOTHING
-
-                if _should_strip_left(j, predecessor_lists):
-                    result[row, col + 1] = NOTHING
-                elif successors and j < max(map(node2position.__getitem__, successors)):
-                    result[row, col + 1] = SECTION
-                elif _above_has_successor_to_the_right(i, j, successor_lists):
-                    result[row, col + 1] = PADDING
-                else:
-                    result[row, col + 1] = NOTHING
-
-                if successors and j < max(map(node2position.__getitem__, successors)):
-                    result[row, col + 2] = SECTION
-                    result[row, col + 3] = SECTION
-                elif _above_has_successor_to_the_right(i, j, successor_lists):
-                    result[row, col + 2] = PADDING
-                    result[row, col + 3] = PADDING
-                else:
-                    result[row, col + 2] = NOTHING
-                    result[row, col + 3] = NOTHING
-
-    return result
-
-
-def _fmt_cells(cells: NDArray, fmt: Callable[[HashableT], str]) -> Iterator[str]:
-    num_row, num_col = cells.shape
-    col2width = {
-        4 * row + 2: len(fmt(cells[row, 4 * row + 2])) for row in range(num_row)
-    }
-    for row in range(num_row):
+    for row, row_node in enumerate(order):
         if row:
+            yield None
+        for col, col_node in enumerate(order):
+            yield cell(row, col, dpred_lists, dsucc_lists, row_node)
+
+
+def _above_has_direct_predecessor_to_the_right(row, col, dsucc_lists):
+    return col in dsucc_lists[row]
+
+
+def _above_has_direct_predecessor_after(row, col, dpred_lists):
+    direct_predecessors = dpred_lists[col]
+    return direct_predecessors and row < max(direct_predecessors)
+
+
+def _right_has_direct_successor_above_or_before(row, col, dsucc_lists):
+    direct_successors = dsucc_lists[row]
+    return direct_successors and min(direct_successors) <= col
+
+
+def _right_has_direct_successor_above_this(row, col, dsucc_lists):
+    return col in dsucc_lists[row]
+
+
+def _has_direct_predecessor(row, col, dpred_lists):
+    assert row == col
+    return dpred_lists[row]
+
+
+def _has_direct_successor_before(row, col, dsucc_lists):
+    assert row == col
+    direct_successors = dsucc_lists[row]
+    return direct_successors and max(direct_successors) < row
+
+
+def _before_has_direct_successor_after(row, col, dsucc_lists):
+    successors = itertools.chain.from_iterable(
+        dsucc_lists.get(node, []) for node in range(row)
+    )
+    try:
+        return col < max(successors)
+    except ValueError:
+        return False
+
+
+def _below_has_direct_predecessor_to_the_left(row, col, dsucc_lists):
+    return col in dsucc_lists[row]
+
+
+def _below_has_direct_predecessore_before(row, col, dpred_lists):
+    direct_predecessors = dpred_lists[col]
+    return direct_predecessors and min(direct_predecessors) < row
+
+
+def _left_has_direct_successor_after(row, col, dsucc_lists):
+    direct_successors = dsucc_lists[row]
+    return direct_successors and col < max(direct_successors)
+
+
+def cell(row, col, dpred_lists, dsucc_lists, node):
+    # before: any node in the quadrant II w.r.t. the current cell
+    # after: any node in the quadrant IV w.r.t. the current cell
+    # above: the node on the diagonal directly above the current cell
+    # below: the node on the diagonal directly below the current cell
+    # left: the node on the diagonal directly to the left of the current cell
+    # right: the node on the diagonal directly to the right of the current cell
+    if col < row:
+        # bottom left half
+        if _above_has_direct_predecessor_to_the_right(row, col, dsucc_lists):
+            ll = INTERSECTION
+        elif _above_has_direct_predecessor_after(row, col, dpred_lists):
+            ll = OVERPASS
+        elif _right_has_direct_successor_above_or_before(row, col, dsucc_lists):
+            ll = SECTION
+        elif col or dpred_lists[col]:
+            ll = PADDING
+        else:
+            ll = NOTHING
+
+        if _right_has_direct_successor_above_or_before(row, col, dsucc_lists):
+            lr = SECTION
+        elif col or dpred_lists[col]:
+            lr = PADDING
+        else:
+            lr = NOTHING
+
+        if _right_has_direct_successor_above_or_before(row, col, dsucc_lists):
+            c = r = SECTION
+        else:
+            c = r = PADDING
+
+    elif row == col:
+        if _has_direct_predecessor(row, col, dpred_lists):
+            ll = INTERSECTION
+            lr = SECTION
+        elif _has_direct_successor_before(row, col, dsucc_lists):
+            ll = lr = SECTION
+        elif col or dpred_lists[col]:
+            ll = lr = PADDING
+        else:
+            ll = lr = NOTHING
+
+        c = node
+
+        if _left_has_direct_successor_after(row, col, dsucc_lists):
+            r = SECTION
+        elif _before_has_direct_successor_after(row, col, dsucc_lists):
+            r = PADDING
+        else:
+            r = NOTHING
+    else:
+        # top right half
+        if _below_has_direct_predecessor_to_the_left(row, col, dsucc_lists):
+            ll = INTERSECTION
+        elif _below_has_direct_predecessore_before(row, col, dpred_lists):
+            ll = OVERPASS
+        elif _left_has_direct_successor_after(row, col, dsucc_lists):
+            ll = SECTION
+        elif _before_has_direct_successor_after(row, col, dsucc_lists):
+            ll = PADDING
+        else:
+            ll = NOTHING
+
+        if _left_has_direct_successor_after(row, col, dsucc_lists):
+            lr = c = r = SECTION
+        elif _before_has_direct_successor_after(row, col, dsucc_lists):
+            lr = c = r = PADDING
+        else:
+            lr = c = r = NOTHING
+
+    return Cell(ll, lr, c, r)
+
+
+def _fmt_cells(
+    cells: Iterable[Optional[Cell]], col2width, fmt: Callable[[HashableT], str]
+) -> Iterator[str]:
+    row = col = 0
+    for cell in cells:
+        if cell is None:
+            row += 1
+            col = 0
             yield "\n"
-        for col in range(num_col):
-            cell = cells[row, col]
-            width = col2width.get(col, 1)
-            if cell is NOTHING:
-                yield ""
-            elif cell in {PADDING, SECTION}:
-                yield cell * width
-            elif cell in {INTERSECTION, OVERPASS}:
-                assert width == 1
-                yield cell
-            else:
-                yield f"{fmt(cell)[:width]:.^{width}}"
+        else:
+            yield _fmt_cell(cell, col2width.get(col, 1), fmt)
+            col += 1
+
+
+def _fmt_cell(cell, width, fmt):
+    return "".join(
+        [
+            _fmt_subcell(cell.ll, 1, fmt),
+            _fmt_subcell(cell.lr, 1, fmt),
+            _fmt_subcell(cell.cc, width, fmt),
+            _fmt_subcell(cell.rr, 1, fmt),
+        ]
+    )
+
+
+def _fmt_subcell(subcell: Union[HashableT, Token], width, fmt):
+    if subcell is NOTHING:
+        return ""
+    elif subcell in {PADDING, SECTION}:
+        return subcell * width
+    elif subcell in {INTERSECTION, OVERPASS}:
+        assert width == 1
+        return subcell
+    else:
+        return f"{fmt(subcell)[:width]:.^{width}}"
